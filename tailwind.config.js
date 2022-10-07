1e7524ac4ed4eba08bce01447302b386644ad5de
Damian <ddanner97@gmail.com>
2022-10-07 15:18:17 -0500

    Extend colors and add SVG

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./src/components/**/*.{js,jsx}", 
+    "./src/components/buttons/**/*.{js,jsx}", 
+    "./src/components/inputs/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EFE7E7",
+        secondary: "#262523",
+        gold: "DEBB86"
+      },
+      fontFamily: {
+        poppins: ["Roboto", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
